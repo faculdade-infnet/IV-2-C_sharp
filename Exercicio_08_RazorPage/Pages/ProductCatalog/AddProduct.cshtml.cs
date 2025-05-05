83bc02f0febf4ba5bc93c01abd538113593fbc57
Samuel Hermany <samuelhermany@hotmail.com>
2025-05-05 20:36:39 -0300

    TP1 - Até o exercício 10

---
+++ Exercicio_08_RazorPage/Pages/ProductCatalog/AddProduct.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.Json;
+
+namespace Exercicio_08_RazorPage.Pages.ProductCatalog
+{
+    public class AddProductModel : PageModel
+    {
+        [BindProperty]
+        public string Nome { get; set; } = string.Empty;
+
+        [BindProperty]
+        public double? Preco { get; set; }
+
+        public void OnGet()
+        {
+        }
+    }
+}
