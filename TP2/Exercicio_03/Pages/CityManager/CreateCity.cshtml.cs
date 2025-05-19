4221729b56423330c4287acfef134e22e493fd5b
Samuel Hermany <samuelhermany@hotmail.com>
2025-05-19 17:04:07 -0300

    TP2 - Exercicio 03

---
+++ TP2/Exercicio_03/Pages/CityManager/CreateCity.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace Exercicio_02.Pages.CityManager
+{
+    public class CreateCityModel : PageModel
+    {
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string CityName { get; set; }
+
+        public class InputModel
+        {
+            [Required(ErrorMessage = "O nome da cidade é obrigatório.")]
+            [MinLength(3, ErrorMessage = "O nome da cidade deve ter no mínimo 3 caracteres.")]
+            public string CityName { get; set; }
+        }
+
+        public void OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                CityName = Input.CityName;
+            }
+        }
+    }
+}
