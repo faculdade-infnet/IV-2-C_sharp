8c6364becceecf4d8e7d906e6bd6f5ec4e6e1ad6
Samuel Hermany <samuelhermany@hotmail.com>
2025-05-19 22:55:12 -0300

    TP2 - Exercicio 07

--- TP2/Exercicio_06/Pages/CreateCountry.cshtml.cs
+++ TP2/Exercicio_06/Pages/CreateCountry.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.Metrics;
 
-namespace Exercicio_05.Pages
+namespace Exercicio_06.Pages
 {
     public class CreateCountryModel : PageModel
     {        
