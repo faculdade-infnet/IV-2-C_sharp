c25f8dba2cd6e4dec3e1b6af76c426e5e5848a0e
Samuel Hermany <samuelhermany@hotmail.com>
2025-06-04 23:09:04 -0300

    TP3 - Até Exercicio 07

---
+++ TP3/CityBreaks.Web07/Migrations/20250605012624_SeedInitialData.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CityBreaks.Web02.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitialData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CountryName",
+                table: "Countries",
+                newName: "Name");
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "CountryCode", "Name" },
+                values: new object[,]
+                {
+                    { 1, "PT", "Portugal" },
+                    { 2, "FR", "França" },
+                    { 3, "BR", "Brasil" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "CountryId", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Lisboa" },
+                    { 2, 1, "Porto" },
+                    { 3, 2, "Paris" },
+                    { 4, 3, "Rio de Janeiro" },
+                    { 5, 3, "São Paulo" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Properties",
+                columns: new[] { "Id", "CityId", "Name", "PricePerNight" },
+                values: new object[,]
+                {
+                    { 1, 1, "Casa no Centro de Lisboa", 120.00m },
+                    { 2, 2, "Apartamento no Porto", 90.00m },
+                    { 3, 3, "Apartamento em Paris", 200.00m },
+                    { 4, 4, "Cobertura no Rio", 150.00m },
+                    { 5, 5, "Flat em São Paulo", 100.00m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Properties",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Properties",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Properties",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Properties",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Properties",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Countries",
+                newName: "CountryName");
+        }
+    }
+}
---
+++ TP3/CityBreaks.Web07/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+using CityBreaks.Web.Models;
+using CityBreaks.Web06.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CityBreaks.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly ICityService _cityService;
+
+        public IndexModel(ILogger<IndexModel> logger, ICityService cityService)
+        {            
+            _cityService = cityService;
+        }
+
+        public List<City> Cities { get; set; } = new();
+
+        public async Task OnGetAsync()
+        {
+            Cities = await _cityService.GetAllAsync();
+        }
+    }
+}
