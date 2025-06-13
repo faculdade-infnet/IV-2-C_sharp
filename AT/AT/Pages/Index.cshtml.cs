4697dc6beb76d570824426c548cd2b2612d7f720
Samuel Hermany <samuelhermany@hotmail.com>
2025-06-13 19:35:30 -0300

    Falta organizar a estrutrua de pastas

--- AT/AT/Pages/Index.cshtml.cs
+++ AT/AT/Pages/Index.cshtml.cs
@@ -1,21 +1,30 @@
-using AT.Data;
-using AT.Services;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AT.Data;
+using AT.Models;
 
-namespace AT.Pages
+namespace AT.Pages.Clientes
 {
     public class IndexModel : PageModel
     {
-        private readonly IService _cityService;
-        private readonly AgenciaContext _context;
+        private readonly AT.Data.AgenciaContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger, AgenciaContext context)
+        public IndexModel(AT.Data.AgenciaContext context)
         {
-            var pacotes = context.PacotesTuristicos.ToList();
-            foreach (var p in pacotes)
-            {
-                Console.WriteLine($"{p.Titulo} - {p.DataInicio}");
-            }
+            _context = context;
+        }
+
+        public IList<Cliente> Cliente { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Cliente = await _context.Clientes
+                .Include(c => c.Endereco).ToListAsync();
         }
     }
 }
--- AT/README.md
+++ AT/README.md
@@ -1,7 +1,14 @@
 # Fundamentos de Desenvolvimento com C#
 # AT - Questões (12)
 Modo de Uso:
-- No projeto existe um arquivo "Program.cs", que é o arquivo principal do projeto.
+- Item5 - Página Destino.
+- Item6 - Página Cliente>Create.
+- Item7 - A clicar enm Detalhes do Cliente ou ao clicar em um Pacote Turistico ja cadastrado na página Create PacoteTurístico.
+- Item8 - Páginas SaveNotes e ViewNotes.
+- Item9 - Arquivo Data>AgenciaContext.cs com database em SQLServer.
+- Item10 - Pasta Migrations com os registros de migração do banco de dados.
+- Item11 - pasta Pages>Cliente estão as operações de CRUD (Create, Edit, Delete)
+Na página Pages>Index esta a operação(Read) do CRUD
 - Para executar o código, basta abrir o arquivo Program.cs e descomentar o exercício que deseja executar e Iniciar a aplicação.
 
 ![Descrição](documentos/enunciado_AT.png)
